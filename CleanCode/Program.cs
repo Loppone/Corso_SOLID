ecfdc18188f91ec6616c9d6c0935eda9a74500d5
Max <maximiliano.bertoli@ff3d.com>
2023-01-30 11:03:59 +0100

    Aggiungere i file di progetto.

---
+++ CleanCode/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+public class Customer
+{
+    public string Name { get; internal set; }
+    public string Email { get; internal set; }
+    public bool IsGold { get; internal set; }
+}
+
+public class Car
+{
+    public string Model { get; set; }
+    public decimal ListPrice { get; set; }
+    public decimal CustomerPrice
+    {
+        get { return _customerPrice; }
+    }
+
+    private decimal _customerPrice;
+
+    public Car()
+    {
+
+    }
+
+    public void Sell(Customer customer)
+    {
+        // SRP
+        _customerPrice = NegotiatePrice(customer);
+        SendEmail(customer.Email);
+    }
+
+    private void SendEmail(string email)
+    {
+        Console.WriteLine($"Grazie per aver acquistato l'auto modello {Model}");
+    }
+
+    private decimal NegotiatePrice(Customer customer)
+    {
+        // OCP
+        if (customer.IsGold)
+            return ListPrice - (ListPrice * 30 / 100);
+        else
+            return ListPrice - (ListPrice * 10 / 100);
+    }
+}
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Clear();
+
+        var car = new Car();
+        car.Model = "Tesla";
+        car.ListPrice = 50000;
+
+        var cust = new Customer();
+        cust.Name = "Pippo";
+        cust.Email = "pippo@gmail.com";
+        cust.IsGold = true;
+
+        car.Sell(cust);
+
+        Console.WriteLine($"Auto venduta a {car.CustomerPrice} €.");
+    }
+}
---
+++ CleanCode/Step 2/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace CleanCode_2
+{
+    public class Customer
+    {
+        public string Name { get; internal set; }
+        public string Email { get; internal set; }
+        public bool IsGold { get; internal set; }
+    }
+
+    public interface IPriceNegotiator
+    {
+        decimal NegotiatePrice(decimal listPrice);
+    }
+
+    public class GoldCustomerPriceNegotiator : IPriceNegotiator
+    {
+        public GoldCustomerPriceNegotiator()
+        {
+        }
+
+        public decimal NegotiatePrice(decimal listPrice)
+        {
+            return listPrice - (listPrice * 30 / 100);
+        }
+    }
+
+    public class StandardCustomerPriceNegotiator : IPriceNegotiator
+    {
+        public StandardCustomerPriceNegotiator()
+        {
+        }
+
+        public decimal NegotiatePrice(decimal listPrice)
+        {
+            return listPrice - (listPrice * 10 / 100);
+        }
+    }
+
+    public class Car
+    {
+        public string Model { get; set; }
+        public decimal ListPrice { get; set; } 
+        public decimal CustomerPrice { get; set; }
+
+        private IPriceNegotiator _priceNegotiator;
+
+        public Car(IPriceNegotiator priceNegotiator)
+        {
+            _priceNegotiator = priceNegotiator;
+        }
+
+        public void Sell(Customer customer)
+        {
+            CustomerPrice = _priceNegotiator.NegotiatePrice(ListPrice);
+            SendEmail(customer.Email);
+        }
+
+        private void SendEmail(string email)
+        {
+            Console.WriteLine($"Grazie per aver acquistato l'auto modello {Model}");
+        }
+    }
+
+
+
+    class Program_2
+    {
+        static void Main2(string[] args)
+        {
+            Console.Clear();
+
+            Car car = new Car(new GoldCustomerPriceNegotiator());
+            car.Model = "Tesla";
+            car.ListPrice = 50000;
+
+            var cust = new Customer();
+            cust.Name = "Pippo";
+            cust.Email = "pippo@gmail.com";
+            cust.IsGold = true;
+
+            car.Sell(cust);
+
+            Console.WriteLine($"Auto venduta a {car.CustomerPrice} €.");
+        }
+    }
+}
