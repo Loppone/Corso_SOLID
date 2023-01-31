a76ef8e9a95d57efc1f58e7d030ca04f26c93e51
Max <maximiliano.bertoli@ff3d.com>
2023-01-31 21:50:02 +0100

    ISP

---
+++ CleanCode/Step 6/Isp_Refactoring.cs
@@ -0,0 +1,72 @@
+﻿namespace CleanCode.Step_6B
+{
+    public interface IVehicle
+    {
+        void Accelerate();
+        void Brake();
+    }
+
+    public interface IMotorizedVehicle : IVehicle
+    {
+        void StartEngine();
+    }
+
+    public interface IFlyingVehicle : IVehicle
+    {
+        void Fly();
+    }
+
+    public interface IWaterVehicle : IVehicle
+    {
+        void Swim();
+    }
+
+    public class Car : IMotorizedVehicle
+    {
+        public void Accelerate()
+        {
+            // Accelera
+        }
+
+        public void Brake()
+        {
+            // Frena
+        }
+
+        public void StartEngine()
+        {
+            // Accendi
+        }
+    }
+
+    public class Airplane : IFlyingVehicle
+    {
+        public void Accelerate()
+        {
+            // Accelera
+        }
+
+        public void Brake()
+        {
+            // Frena
+        }
+
+        public void Fly()
+        {
+            // Vola
+        }
+    }
+
+    public class Bycicle : IVehicle
+    {
+        public void Accelerate()
+        {
+            // Pedala
+        }
+
+        public void Brake()
+        {
+            // Frena
+        }
+    }
+}
---
+++ CleanCode/Step 6/Isp_Violation.cs
@@ -0,0 +1,95 @@
+﻿namespace CleanCode.Step_6A
+{
+    public interface IVehicle
+    {
+        void StartEngine();
+        void Accelerate();
+        void Brake();
+        void Fly();
+        void Swim();
+    }
+
+    public class Car : IVehicle
+    {
+        public void StartEngine()
+        {
+            // Accendi
+        }
+
+        public void Accelerate()
+        {
+            // Accelera
+        }
+
+        public void Brake()
+        {
+            // Frena
+        }
+
+        public void Fly()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Swim()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public class Airplane : IVehicle
+    {
+        public void StartEngine()
+        {
+            // Accendi
+        }
+
+        public void Accelerate()
+        {
+            // Accelera
+        }
+
+        public void Brake()
+        {
+            // Frena
+        }
+
+        public void Fly()
+        {
+            // Vola
+        }
+
+        public void Swim()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public class Bycicle : IVehicle
+    {
+        public void StartEngine()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Accelerate()
+        {
+            // Pedala
+        }
+
+        public void Brake()
+        {
+            // Frena
+        }
+
+        public void Fly()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Swim()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
